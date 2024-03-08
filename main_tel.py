bcd32ff7665a26918d6cc8a37c7d5bbdf33e888d
Shashikant Vishwakarma <57395523+iversions@users.noreply.github.com>
2024-03-08 18:25:01 +0530

    Update main_tel.py

--- main_tel.py
+++ main_tel.py
@@ -8,8 +8,6 @@ import pathlib
 import datetime
 import json
 import configparser
-# import logging
-# import logging.config
 import re
 import time
 import sys
@@ -21,7 +19,6 @@ from office365.runtime.client_request_exception import ClientRequestException
 from ast import literal_eval
 from string import ascii_lowercase
 from itertools import groupby
-# import pandas as pd
 from office365.sharepoint.files.move_operations import MoveOperations
 from airtel_ext import airext
 from mtnl_ext import mtnlext
@@ -41,6 +38,12 @@ sppassword = spparam['upass']
 cid = spparam['cid']
 cs = spparam['cs']
 
+sproot_air = sppaths['root_for_air']
+sproot_tel = sppaths['root_for_tel']
+
+spprocessed_air = sppaths['processed_for_air']
+spprocessed_tel = sppaths['processed_for_tel']
+
 sproot = sppaths['root']
 spprocessed = sppaths['processed']
 spproblematic = sppaths['problematic']
@@ -53,25 +56,70 @@ sprproot= sprlpath['root']
 sprpproblem = sprlpath['problematic']
 sprpduplicate = sprlpath['duplicate'] ###Add duplicate path
 
-def move_to_folder_processed(folder):
-    file_from = ctx.web.get_folder_by_server_relative_url(folder)
+def main(sproot,spprocessed):
+    global flag,list_of_airlines
+    global ctx
+    print('1')
+
+    root_folder = ctx.web.get_folder_by_server_relative_path(sproot)
+    pro_folder = ctx.web.get_folder_by_server_relative_path(spprocessed)
+
+    root_folder.expand(["Folders"]).get().execute_query()
+    metaurl = ''
+
+    if root_folder.folders:
+        print(root_folder.folders)
+        for folder in root_folder.folders:
+            folder = try_get_folder(folder.serverRelativeUrl)
+            files = folder.get_files(True).execute_query()
+            for f in files:
+                text =''
+                metaurl = f.properties['ServerRelativeUrl']
+                finalurl = spsite+metaurl
+                file_name = os.path.basename(finalurl)
+                path = os.path.join(lsppath, file_name)
+
+                print('>>>>>>>>>>>>>>>>',path)
+                with open(path,'wb') as local_file:
+                    p_file = ctx.web.get_file_by_server_relative_url(metaurl).download(local_file).execute_query()
+            
+                if '.pdf' in file_name or '.PDF' in file_name:
+                    print('pdf',file_name)
+                    with pdfplumber.open(path) as pdf:
+                        for page in pdf.pages:                    
+                            text += page.extract_text()
+
+                    #AIRLINE CHECK
+                    for words in list_of_airlines:
+                        if words in text:
+                            exec(open(r'D:\Airline Invoice Code\Airline_Invoice_Extraction.py').read(), {'path': path })
+                            flag = 1
+                            break
+                    
+                    #TELEPHONE CHECK
+                    if "Bharti Airtel" in text:
+                        exec(open(r'D:\Airline Invoice Code\airtel_bill.py').read(), {'path': path })  
+                        flag = 0
+                    if "Mahanagar" in text:
+                        print('IT is MTNL')
+                        exec(open(r'D:\Airline Invoice Code\MTNL_pdfplumber.py').read(), {'path': path })
+                        flag = 2
+                
+                pdf.close()
+                time.sleep(5)
+                os.remove(path)
+            if flag == 1:
+                move_to_folder_processed(folder.serverRelativeUrl,sprppro_air)
+            if flag == 2:
+                move_to_folder_processed(folder.serverRelativeUrl,sprppro_tel)
+    
+
+def move_to_folder_processed(folder,sprppro):
+    file_from = ctx.web.get_folder_by_server_relative_url(folder).execute_query()
 
     file_to = file_from.move_to(sprppro).execute_query()
     print("'{0}' moved into '{1}'".format(folder, sprppro))
 
-def try_get_folder(url):
-    try:
-        return ctx.web.get_folder_by_server_relative_url(url).get().execute_query()
-    except ClientRequestException as e:
-        if e.response.status_code == 404:
-            return None
-        else:
-            raise ValueError(e.response.text)
-        
-ctx = ClientContext(spdoclib).with_credentials(ClientCredential(cid, cs))
-
-root_folder = ctx.web.get_folder_by_server_relative_path(sproot)
-pro_folder = ctx.web.get_folder_by_server_relative_path(spprocessed)
                                                                                                                                                         
 def try_get_folder(url):
     try:
@@ -81,45 +129,7 @@ def try_get_folder(url):
             return None
         else:
             raise ValueError(e.response.text)
-        
-root_folder.expand(["Folders"]).get().execute_query()
-metaurl = ''
 
-for folder in root_folder.folders:
-    folder = try_get_folder(folder.serverRelativeUrl)
-    files = folder.get_files(True).execute_query()
-    print(folder)
-    for f in files:
-        text =''
-        words =[]
-        metaurl = f.properties['ServerRelativeUrl']
-        finalurl = spsite+metaurl
-        file_name = os.path.basename(finalurl)
-        path = os.path.join(lsppath, file_name)
-
-        with open(path,'wb') as local_file:
-            p_file = ctx.web.get_file_by_server_relative_url(metaurl).download(local_file).execute_query()
     
-        if '.pdf' in file_name or '.PDF' in file_name:
-            print('pdf',file_name)
-            with pdfplumber.open(path) as pdf:
-                for page in pdf.pages:                    
-                    page_words = page.extract_words()
-                    words.extend([word['text'] for word in page_words])
-                    text += page.extract_text()
-            
-            if "Bharti Airtel" in text:
-                airext(path)
-                # exec(open(r'D:\TELEPHONE_BILLS\airtel_bill.py').read(), {'path': path })    
-            if "Mahanagar" in text:
-                mtnlext(path)
-                print('IT is MTNL')
-                # exec(open(r'D:\TELEPHONE_BILLS\MTNL_pdfplumber.py').read(), {'path': path })
-        time.sleep(5)
-        os.remove(file_name)
-    move_to_folder_processed(folder.serverRelativeUrl)
-    print('Moved folder:', folder)
-
-
-
-
+main(sproot_air,spprocessed_air)
+main(sproot_tel,spprocessed_tel)
