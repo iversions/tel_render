99dc2f476be21423d27f29d0cff13979461a78db
parrrthhh <105983295+parrrthhh@users.noreply.github.com>
2024-03-11 14:18:33 +0530

    Update airtel_ext.py

--- airtel_ext.py
+++ airtel_ext.py
@@ -114,7 +114,7 @@ def airext (path):
     if duplicate ==  0:
         status = 'PROCCESSED'
 
-    pdf.close()
+    
     insert_into_main_table('AIRTEL',status,employee_name,bill_no,bill_period_from,bill_period_to,bill_date,telephone_no,bill_amount,cgst,sgst,total_amount,path,taxes,taxable,non_taxable,ship_to_state_code,supply)
 
 
