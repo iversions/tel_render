da39f912afc9efd7a21a793709225147827b6a1f
parrrthhh <105983295+parrrthhh@users.noreply.github.com>
2024-03-20 10:20:43 +0530

    Update mtnl_ext.py

--- mtnl_ext.py
+++ mtnl_ext.py
@@ -57,28 +57,31 @@ def mtnlext (path):
     #print('List is:',z)
 
 
-    e = 0
-    for w in z:
-        #print(o,'   ',z[p:p+2])
-        if  z[e] == 'Name:':
-            #print('Bill Period is:', z[p+2:])
-            aaa = z[e+1:]
-            name1 = 0
-            yyy = []
-            for aa in aaa:
-                if aaa[name1] == 'Installation':
-                    cc = name1
-                    employee_name = " ".join(str(i) for i in aaa[:cc])
-                    
-                name1+=1
-            break
-        if (e+2)<= len(z):
-            e+=1
-
-
-
-
-
+    try:
+        e = 0
+        for w in z:
+            #print(o,'   ',z[p:p+2])
+            if  z[e] == 'Name:':
+                #print('Bill Period is:', z[p+2:])
+                aaa = z[e+1:]
+                name1 = 0
+                yyy = []
+                for aa in aaa:
+                    if aaa[name1] == 'Installation':
+                        cc = name1
+                        employee_name = " ".join(str(i) for i in aaa[:cc])
+                        
+                    name1+=1
+                break
+            if (e+2)<= len(z):
+                e+=1
+        print(employee_name)
+    except Exception as e:
+        employee_name ='-'
+    
+    
+    
+    
     o = 0
     for w in z:
         #print(o,'   ',z[o:o+2])
@@ -89,88 +92,105 @@ def mtnlext (path):
         if (o+2)<= len(z):
             o+=1
     
-    bill_no = z[o+3]
-    print('Bill No is:', bill_no)
+    try:
+        bill_no = z[o+3]
+        print('Bill No is:', bill_no)
+    except Exception as e:
+        bill_no ='-'
     print('\n')
-    bill_period_from = z[o+13]
-    bill_period_to = z[o+15] 
-    print('Bill Period is:',bill_period_from, 'to' ,bill_period_to)
+    
+    try:
+        bill_period_from = z[o+13]
+        bill_period_to = z[o+15] 
+        print('Bill Period is:',bill_period_from, 'to' ,bill_period_to)
+    except Exception as e:
+        bill_period_to,bill_period_from = '-','-'
+    
     print('\n')
-    bill_date =z[o+16]
-    print('Bill Date is:', bill_date)
+    try:
+        bill_date =z[o+16]
+        print('Bill Date is:', bill_date)
+    except Exception as e:
+        bill_date ='-'
+    
+    
     print('\n')
-    telephone_no = z[o+12]
-    print('Telephone Number is:', telephone_no)
-
-
-
-
+    try:
+        telephone_no = z[o+12]
+        print('Telephone Number is:', telephone_no)
+    except Exception as e:
+        telephone_no ='-'
+    
+    
+    
     print('\n')
-
-
-
-
-    r = 0
-    for w in z:
-        #print(r,'   ',z[r:r+2])
-        if " ".join(str(i) for i in  z[r:r+3]) == 'Total Taxable Value':
-            bill_amount = z[r+3]
-            print('Bill Amount is:', bill_amount)
-            aaa = z[r+2]
-        if (r+2)<= len(z):
-            r+=1
-
-
-
-
-
+    
+    
+    
+    try:
+        r = 0
+        for w in z:
+            #print(r,'   ',z[r:r+2])
+            if " ".join(str(i) for i in  z[r:r+3]) == 'Total Taxable Value':
+                bill_amount = z[r+3]
+                print('Bill Amount is:', bill_amount)
+                aaa = z[r+2]
+            if (r+2)<= len(z):
+                r+=1
+        print(bill_amount)
+    except Exception as e:
+        bill_amount ='-'
     print('\n')
-
-
-
-
-
-
-
-    t = 0
-    for w in z:
-        #print(r,'   ',z[r:r+2])
-        if " ".join(str(i) for i in  z[t:t+3]) == 'CGST @ 9%':
-            cgst = z[t+3]
-            sgst = z[t+3]
-            print('CGST is:',cgst )
-            print('SGST is:',sgst )
-            aaa = z[t+2]
-        if (t+2)<= len(z):
+    
+    try:
+        t = 0
+        for w in z:
+            #print(r,'   ',z[r:r+2])
+            if " ".join(str(i) for i in  z[t:t+3]) == 'CGST @ 9%':
+                cgst = z[t+3]
+                sgst = z[t+3]
+                print('CGST is:',cgst )
+                print('SGST is:',sgst )
+                aaa = z[t+2]
+            if (t+2)<= len(z):
+                t+=1
+        print(cgst,sgst)
+    except Exception as e:
+        cgst ='-'
+        sgst ='-'
+    
+    try:
+        t = 0
+        for w in z:
+            #print(r,'   ',z[r:r+2])
+            if " ".join(z[t:t+4]) == 'Other Non Taxable Credit':
+                non_taxable = z[t+4]
+                break
             t+=1
-
-
-
-    t = 0
-    for w in z:
-        #print(r,'   ',z[r:r+2])
-        if " ".join(z[t:t+4]) == 'Other Non Taxable Credit':
-            non_taxable = z[t+4]
-            break
-        t+=1
-
-    t = 0
-    for w in z:
-        #print(r,'   ',z[r:r+2])
-        if " ".join(z[t:t+3]) == 'Total Taxable Value':
-            taxable = z[t+3]
-            break
-        t+=1
-
+        print(non_taxable)
+    except Exception as e:
+        non_taxable = '-'
+    
+    try:    
+        t = 0
+        for w in z:
+            #print(r,'   ',z[r:r+2])
+            if " ".join(z[t:t+3]) == 'Total Taxable Value':
+                taxable = z[t+3]
+                break
+            t+=1
+        print(taxable)
+    except Exception as e:
+        taxable ='-'
+    
     print('\n')
-    total_amount = z[o+5]
-    print('Total Amount is:', total_amount)
-
-
-
-    print('Employee Name is :', employee_name)
-
-    print(non_taxable)
+    
+    try:
+        total_amount = z[o+5]
+        print('Total Amount is:', total_amount)
+    except Exception as e:
+        total_amount = '-'
+    
 
     ship_to_state_code,supply = '-','-'
     taxes = '-'
