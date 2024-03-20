781b90a1de67c24d059993989fa17aea575dd2e9
parrrthhh <105983295+parrrthhh@users.noreply.github.com>
2024-03-20 10:18:21 +0530

    Update airtel_ext.py

--- airtel_ext.py
+++ airtel_ext.py
@@ -29,79 +29,113 @@ def airext (path):
         all_text.extend(lines)
 
 
-    for line in all_text:
-        if "Billing Address" in line:
-            employee_name = all_text[all_text.index("Billing Address") + 1 ]
-    print(employee_name)
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+4]) == 'Ship To State Code':
-            ship_to_state_code = words[a+5]
-            break
-        a+=1
-    print(ship_to_state_code)
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+2]) == 'Bill NO':
-            bill_no = words[a+2]
-            break
-        a+=1
-    print(bill_no)
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+2]) == 'Bill Period':
-            bill_period_from = ' '.join(words[a+2:a+7]).split('-')[0]
-            bill_period_to = ' '.join(words[a+2:a+7]).split('-')[1]
-            break
-        a+=1
-    print(bill_period_from,bill_period_to)
-
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+2]) == 'Bill Date':
-            bill_date = ' '.join(words[a+2:a+5])
-            break
-        a+=1
-    print(bill_date)
-
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+2]) == 'Rental Charges':
-            bill_amount = words[a+2]
-            break
-        a+=1
-    print(bill_amount)
-
-    a = 0
-    for word in words:
-        if words[a] == 'Taxes' and ' '.join(words[a+2:(a+4)]) == 'Total Amount':
-            taxes = words[a+1]
-            total_amount = words[a+5]
-            break
-        a+=1
-    print(taxes,total_amount)
-
-    a = 0
-    for word in words:
-        if words[a] == 'PhoneNo:':
-            telephone_no = words[a+1]
-            break
-        a+=1
-    print(telephone_no)
-
-    a = 0
-    for word in words:
-        if ' '.join(words[a:a+4]) == 'Place of Supply :':
-            supply = words[a+4]
-            break
-        a+=1
-    print(supply)
-
+    try:
+        for line in all_text:
+            if "Billing Address" in line:
+                employee_name = all_text[all_text.index("Billing Address") + 1]
+        print(employee_name)
+    except Exception as e:
+        employee_name ='-'
+    
+    
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+4]) == 'Ship To State Code':
+                ship_to_state_code = words[a+5]
+                break
+            a+=1
+        print(ship_to_state_code)
+    except Exception as e:
+        ship_to_state_code ='-'
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+2]) == 'Bill NO':
+                bill_no = words[a+2]
+                break
+            a+=1
+        print(bill_no)
+    except Exception as e:
+        bill_no ='-'
+    
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+2]) == 'Bill Period':
+                bill_period_from = ' '.join(words[a+2:a+7]).split('-')[0]
+                bill_period_to = ' '.join(words[a+2:a+7]).split('-')[1]
+                break
+            a+=1
+        print(bill_period_from,bill_period_to)
+    except Exception as e:
+        try:
+            a = 0
+            for word in words:
+                if ' '.join(words[a:a+2]) == 'Bill Period':
+                    bill_period_from = ' '.join(words[a+2:a+9]).split('to')[0]
+                    bill_period_to = ' '.join(words[a+2:a+9]).split('to')[1]
+                    break
+                a+=1
+            print(bill_period_from,bill_period_to)
+        except Exception as e:
+            bill_period_from='-'
+            bill_period_to ='-'
+    
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+2]) == 'Bill Date':
+                bill_date = ' '.join(words[a+2:a+5])
+                break
+            a+=1
+        print(bill_date)
+    except Exception as e:
+        bill_date ='-'
+    
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+2]) == 'Rental Charges':
+                bill_amount = words[a+2]
+                break
+            a+=1
+        print(bill_amount)
+    except Exception as e:
+        bill_amount ='-'
+    
+    try:
+        a = 0
+        for word in words:
+            if words[a] == 'Taxes' and ' '.join(words[a+2:(a+4)]) == 'Total Amount':
+                taxes = words[a+1]
+                total_amount = words[a+5]
+                break
+            a+=1
+        print(taxes,total_amount)
+    except Exception as e:
+        taxes ='-'
+        total_amount ='-'
+    
+    
+    try:
+        for line in all_text:
+            if 'PhoneNo' in line:
+                telephone_no = line.split(':')[1]
+        print('>>> ',telephone_no)
+    except Exception as e:
+        telephone_no ='-'
+    
+    try:
+        a = 0
+        for word in words:
+            if ' '.join(words[a:a+4]) == 'Place of Supply :':
+                supply = words[a+4]
+                break
+            a+=1
+        print(supply)
+    except Exception as e:
+        supply = '-'
     cgst ='-'
     sgst ='-'
     taxable ='-'
